44b6c1ad25ded268bf5963c2369fd60313f1f1cd
Luis Suárez <luis.suarez.menchu@gmail.com>
2026-07-08 11:46:26 -0600

    Agregar mi parte Luios
    Agregue 20 lineas de puuuura cosa x.

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,24 @@
 print ('hola mucha ponganse a hacer algo')
+
+
+# lUISITO EL NENE
+
+# Variable
+nombre = "Luis"
+
+# Mostrar en consola
+print("Hola " + nombre)
+
+# Condición
+if nombre == "Luis":
+    print("Bienvenido")
+
+# Ciclo
+for i in range(1, 6):
+    print(i)
+
+# Función
+def sumar(a, b):
+    return a + b
+
+print(sumar(3, 5))
